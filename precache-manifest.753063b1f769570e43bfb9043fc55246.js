932d3134bb1b6f3331163e7ff789dd9dad5ddd76
Mitchell Freeman <mitchell.w.freeman13@gmail.com>
2022-05-31 22:57:32 -0400

    Updates

---
+++ precache-manifest.753063b1f769570e43bfb9043fc55246.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e14bb58bef441cd2c6cd",
+    "url": "/static/css/main.0dff4062.chunk.css"
+  },
+  {
+    "revision": "e14bb58bef441cd2c6cd",
+    "url": "/static/js/main.e14bb58b.chunk.js"
+  },
+  {
+    "revision": "7434611c6f407bbd86a9",
+    "url": "/static/js/1.7434611c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8185191e32f68b18f9209d444fe1d7ae",
+    "url": "/index.html"
+  }
+];
